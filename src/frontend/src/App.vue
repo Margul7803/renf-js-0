d3fcc5eba64e8dc3cf62bc61098af1c3363c10ab
mario <mario.aboujamra@weblib.eu>
2024-09-18 16:27:55 +0200

    ♻️: refaco component/page structure

--- src/frontend/src/App.vue
+++ src/frontend/src/App.vue
@@ -1,7 +1,26 @@
-<script setup>
-import Home from './pages/Home.vue'
-</script>
-
 <template>
-  <Home />
+  <nav class="header">
+    <h1 style="margin-bottom: 100px;">Welcom to youre taskManager™ dashboard !</h1>
+    <div class="nav-btn">
+      <v-btn @click='$router.push("/")'>Home</v-btn>
+      <v-btn @click='$router.push("/add-tasks")'>Add Tasks</v-btn>
+    </div>
+  </nav>
+  <router-view />
+
 </template>
+
+<style scoped>
+.header {
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  margin: 20px;
+}
+
+.nav-btn {
+  display: flex;
+  justify-content: space-between;
+  width: 240px;
+}
+</style>
